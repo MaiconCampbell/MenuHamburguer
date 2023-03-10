ca1f66ca5be4f6e6744ad41f1993a35947d343b6
Maicon Campbell <maicon_campbell@hotmail.com>
2023-03-09 22:45:23 -0300

    projeto Menu Hamburguer - Responsividade

--- js/query.js
+++ js/query.js
@@ -1,5 +1,15 @@
+const $elBody = document.querySelector('body');
 const $elBurguer = document.querySelector('#burguer')
 const $elItensMenu = document.querySelector('#itensMenu')
+
+window.addEventListener('resize', () => {
+  //var altura = window.innerHeight;
+  var largura = window.innerWidth
+console.log(largura)
+  largura >=768 ?
+  $elItensMenu.style.display = 'block' :
+  $elItensMenu.style.display = 'none'
+})
 
 $elBurguer.addEventListener('click', () => {
   $elItensMenu.style.display == 'block' ?
